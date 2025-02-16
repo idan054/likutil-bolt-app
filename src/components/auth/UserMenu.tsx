d4fa7fb64bfb5925371aaa8b560d881427a7a757
BitonApp <idanbit80@gmail.com>
2025-02-16 08:46:15 +0200

    (:

--- src/components/auth/UserMenu.tsx
+++ src/components/auth/UserMenu.tsx
@@ -13,16 +13,25 @@ export const UserMenu: React.FC<UserMenuProps> = ({ user, onOpenSettings, storeU
     
 
     
-    <span className="text-sm text-gray-700">
-      {storeUrl || user.email}
-    </span>
-
-    <img
-        // src={user.photoURL}
-        src={`https://www.google.com/s2/favicons?domain=${storeUrl}&sz=64`}
-        alt={user.displayName || 'User avatar'}
-        className="w-8 h-8 rounded-full"
-      />
+    <a 
+      href={storeUrl}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="group flex items-center gap-3 hover:opacity-80 transition-all duration-300"
+    >
+      
+      <div className=" h-10 rounded-full border-2 border-gray-200 flex items-center justify-center overflow-hidden mx-2 hover:border-blue-500 transition-all duration-300 shadow-md hover:shadow-lg transform hover:scale-110">
+      <span className="text-sm font-medium text-gray-700 truncate max-w-[150px] group-hover:text-blue-600 transition-colors pr-2">
+        {storeUrl || user.email}
+      </span>
+      
+        <img
+          src={`https://www.google.com/s2/favicons?domain=${storeUrl}&sz=64`}
+          alt={user.displayName || 'User avatar'}
+          className="h-full object-cover p-2"
+        />
+      </div>
+    </a>
 
     <button
       onClick={onOpenSettings}
