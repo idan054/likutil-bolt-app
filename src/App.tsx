7195f34d06796ba4aae535817a247d309a8ac2a2
Biton-Boss <idan.biton@mycommuni.com>
2025-03-22 17:39:35 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { DevPage } from './components/dev/DevPage';
 
 
 
-export const APP_VERSION = '12.03.25 | Version 1.1';
+export const APP_VERSION = '22.03.25 | Version 1.1';
 
 // FROM MAC
 export const App: React.FC = () => {
