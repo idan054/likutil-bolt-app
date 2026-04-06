607b98cb2fb2c3170906c12d860a1761b9480b73
Eyalspider3D <eyalspider3d@gmail.com>
2026-04-06 16:18:36 +0300

    Chore: use environment variables for Firebase config to fix Netlify build

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,13 +3,13 @@
 
 // Firebase Config
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDZ609tpVpSrWfOKVHVoyo9DK8dbYqAtRE",
-  authDomain: "likutil.firebaseapp.com",
-  projectId: "likutil",
-  storageBucket: "likutil.firebasestorage.app",
-  messagingSenderId: "314662541049",
-  appId: "1:314662541049:web:9e6efa3430e1be4f47f428",
-  measurementId: "G-VYRKS8Z4EX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 } as const;
 
 // Collection names
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, enableMultiTabIndexedDbPersistence } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZ609tpVpSrWfOKVHVoyo9DK8dbYqAtRE",
-  authDomain: "likutil.firebaseapp.com",
-  projectId: "likutil",
-  storageBucket: "likutil.firebasestorage.app",
-  messagingSenderId: "314662541049",
-  appId: "1:314662541049:web:9e6efa3430e1be4f47f428",
-  measurementId: "G-VYRKS8Z4EX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
